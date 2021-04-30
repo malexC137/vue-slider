af96e0093a2bd2c4e04d9d7beebb630cc56483c8
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-04-30 17:38:48 +0200

    Ex started

---
+++ slider.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        images: [{
+            name: "tokyo",
+            imgSrc: "img/tokyo.jpeg",
+        },  {
+            name: "bangkok",
+            imgSrc: "img/bangkok.jpeg",
+        }, {
+            name: "sydney",
+            imgSrc: "img/sydney.jpeg",
+        }, {
+            name: "singapore",
+            imgSrc: "img/singapore.jpg",
+        }
+    ]
+    }
+})
